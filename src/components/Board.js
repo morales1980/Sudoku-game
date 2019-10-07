96e1e1fc7f7cb164c82c050f348db5ccde3f5fc6
Darek.Szotowicz <dariusz.szotowicz@protonmail.com>
2019-10-07 21:39:15 +0200

    fixed row and column hover functionality

--- src/components/Board.js
+++ src/components/Board.js
@@ -159,35 +159,35 @@ class Board extends Component {
       // const blockEight  = [57, 58, 59, 66, 67, 68, 75, 76, 77];
       // const blockNine   = [60, 61, 62, 69, 70, 71, 78, 79, 80];
 
-      // const data = [
-      //   [0,  1,  2,  9, 10, 11, 18, 19, 20],
-      //   [3,  4,  5, 12, 13, 14, 21, 22, 23],
-      //   [6,  7,  8, 15, 16, 17, 24, 25, 26],
-      //   [27, 28, 29, 36, 37, 38, 45, 46, 47],
-      //   [30, 31, 32, 39, 40, 41, 48, 49, 50],
-      //   [33, 34, 35, 42, 43, 44, 51, 52, 53],
-      //   [54, 55, 56, 63, 64, 65, 72, 73, 74],
-      //   [57, 58, 59, 66, 67, 68, 75, 76, 77],
-      //   [60, 61, 62, 69, 70, 71, 78, 79, 80]
-      // ];
+      const data = [
+        [0,  1,  2,  9, 10, 11, 18, 19, 20],
+        [3,  4,  5, 12, 13, 14, 21, 22, 23],
+        [6,  7,  8, 15, 16, 17, 24, 25, 26],
+        [27, 28, 29, 36, 37, 38, 45, 46, 47],
+        [30, 31, 32, 39, 40, 41, 48, 49, 50],
+        [33, 34, 35, 42, 43, 44, 51, 52, 53],
+        [54, 55, 56, 63, 64, 65, 72, 73, 74],
+        [57, 58, 59, 66, 67, 68, 75, 76, 77],
+        [60, 61, 62, 69, 70, 71, 78, 79, 80]
+      ];
 
 
 
       const value = index;
 
-      // const result = data.find(function(element) {
-      //
-      //   const asd = element.find(function(item) {
-      //     return item === value;
-      //   });
-      //
-      //   return asd;
-      // });
-      // console.log(result);
+      const result = data.find(function(element) {
+
+        const asd = element.find(function(item) {
+          return item === value;
+        });
+
+        return asd;
+      });
+      console.log(result);
       console.log(index);
 
-      getFirstElementInARow(index);
     }
+    getFirstElementInARow(index);
   }
 
   handleMouseLeave(index, e) {
